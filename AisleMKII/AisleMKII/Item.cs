4bb4f957bf841b18cc3ec79a8b00aeda5d42b780
khush kaushal <khushkaushal62@yahoo.com>
2018-08-29 10:41:11 -0700

    fully functional

---
+++ AisleMKII/AisleMKII/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AisleMKII
+{
+    class Item
+    {
+        public string Name { get; set; }
+       public string Aisle { get; set; }
+    }
+}
--- AisleMKII/AisleMKII/MainPage.xaml.cs
+++ AisleMKII/AisleMKII/MainPage.xaml.cs
@@ -9,9 +9,26 @@ namespace AisleMKII
 {
     public partial class MainPage : ContentPage
     {
+        List<Item> _items = new List<Item>
+        {
+            new Item {Name="Shampoo", Aisle="Aisle 15"},
+            new Item {Name="Chicken", Aisle="Aisle 10"},
+            new Item {Name="Fish", Aisle="Aisle 9"},
+            new Item {Name="Cereal", Aisle="Aisle 10"}
+        };
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = _items;
+            
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string keyword = searchBox.Text;
+
+            BindingContext = _items.FindAll(x => x.Name.Contains(keyword));
         }
     }
 }
